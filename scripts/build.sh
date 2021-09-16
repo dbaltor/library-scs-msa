33ec85968417249af4ac69cf4859bf03a51a3540
Denis Baltor <dbaltor@pivotal.io>
2021-09-17 00:55:09 +0100

    Removed unnecessary building steps

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,19 +4,7 @@ cd $(dirname $0)
 
 cd ..
 
-./gradlew clean reader:build -x test
-ret=$?
-if [ $ret -ne 0 ]; then
-  exit $ret
-fi
-
 ./gradlew reader:publishToMavenLocal
-ret=$?
-if [ $ret -ne 0 ]; then
-  exit $ret
-fi
-
-./gradlew clean book:build -x test
 ret=$?
 if [ $ret -ne 0 ]; then
   exit $ret
