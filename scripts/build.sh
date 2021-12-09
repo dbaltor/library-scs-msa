31de8cd7e49b1cde5fdad41cd82981141a2f475d
Denis Baltor <39310315+dbaltor@users.noreply.github.com>
2021-12-09 12:27:05 +0000

    Removed unnecessary checking on return code

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,16 +5,12 @@ cd $(dirname $0)
 cd ..
 
 ./gradlew reader:publishToMavenLocal
-ret=$?
-if [ $ret -ne 0 ]; then
-  exit $ret
-fi
+#ret=$?
+#if [ $ret -ne 0 ]; then
+#  exit $ret
+#fi
 
 ./gradlew book:publishToMavenLocal
-ret=$?
-if [ $ret -ne 0 ]; then
-  exit $ret
-fi
 
 ./gradlew build --parallel
 
